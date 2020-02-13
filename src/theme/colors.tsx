f7041a1ae891f787759d6f44650496397d0ac731
tobiaserich <tobiaserich@hotmail.de>
2020-02-13 16:26:06 +0100

    Add theming

---
+++ src/theme/colors.tsx
@@ -0,0 +1,11 @@
+const normal = {
+  colors: {
+    frogBody: "hsl(126, 48%, 51%)",
+    belly: "hsl(126, 58%, 55%)",
+    legs: "hsl(126, 40%, 43%)",
+    arms: "hsl(126, 45%, 48%)",
+    border: "hsl(126, 45%, 41%)"
+  }
+};
+
+export default normal;
